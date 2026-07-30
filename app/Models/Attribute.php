994e56851629baf4e4ace4a7d355a1cf56d57370
Mustafa Sayed <mustafa.sayed@btu.edu.eg>
2026-07-31 02:14:13 +0300

    إضافة ربط الخصائص (attributes) بالأقسام لدعم اللون/المقاس/الوزن

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -12,10 +12,25 @@ class Attribute extends Model
     protected $fillable = [
         'name',
         'slug',
+        'category_id',
     ];
 
     public function values()
     {
         return $this->hasMany(AttributeValue::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // خاصية عامة تظهر لكل الأقسام (زي "اللون") أو خاصة بقسم واحد بس (زي "الوزن" لقسم المواد الغذائية)
+    public function scopeForCategory($query, $categoryId)
+    {
+        return $query->where(function ($q) use ($categoryId) {
+            $q->whereNull('category_id')
+                ->orWhere('category_id', $categoryId);
+        });
     }
 }
